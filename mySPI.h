a42a46cf74b6a96c89799c42f2535e545c6cbcd5
Ken <tranthamkw@unk.edu>
2018-12-21 10:59:58 -0600

    add winr

--- mySPI.h
+++ mySPI.h
@@ -5,19 +5,9 @@
  * Created on June 30, 2018, 8:03 PM
  */
 
-#ifndef MYSPI_H
-#define	MYSPI_H
 
-#ifdef	__cplusplus
-extern "C" {
-#endif
 
     void initSPI(void);
     unsigned char writeSPI(char data_out);
 
-#ifdef	__cplusplus
-}
-#endif
-
-#endif	/* MYSPI_H */
 
