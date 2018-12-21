96360a23a7e04e8da00de599d4191dc720446788
tranthamkw <tranthamkw@users.noreply.github.com>
2018-12-21 11:05:13 -0600

    Update mySPI.c

--- mySPI.c
+++ mySPI.c
@@ -1,12 +1,94 @@
 
-
+#include <stdint.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <asm/ioctl.h>
+#include <linux/spi/spidev.h>
 #include "mySPI.h"
+// The SPI bus parameters
+//	Variables as they need to be passed as pointers later on
+
+static const char       *spiDev0  = "/dev/spidev0.0" ;
+static const char       *spiDev1  = "/dev/spidev0.1" ;
+static const uint8_t     spiBPW   = 8 ;
+static const uint16_t    spiDelay = 0 ;
+
+static uint32_t    spiSpeeds [2] ;
+static int         spiFds [2] ;
 
 
 
-void initSPI(){
- 
 
+/*
+ * wiringPiSPIDataRW:
+ *	Write and Read a block of data over the SPI bus.
+ *	Note the data ia being read into the transmit buffer, so will
+ *	overwrite it!
+ *	This is also a full-duplex operation.
+ *********************************************************************************
+ */
+
+int wiringPiSPIDataRW (int channel, unsigned char *data, int len)
+{
+  struct spi_ioc_transfer spi ;
+
+  channel &= 1 ;
+
+// Mentioned in spidev.h but not used in the original kernel documentation
+//	test program )-:
+
+  memset (&spi, 0, sizeof (spi)) ;
+
+  spi.tx_buf        = (unsigned long)data ;
+  spi.rx_buf        = (unsigned long)data ;
+  spi.len           = len ;
+  spi.delay_usecs   = spiDelay ;
+  spi.speed_hz      = spiSpeeds [channel] ;
+  spi.bits_per_word = spiBPW ;
+
+  return ioctl (spiFds [channel], SPI_IOC_MESSAGE(1), &spi) ;
+}
+
+
+/*
+ * wiringPiSPISetupMode:
+ *	Open the SPI device, and set it up, with the mode, etc.
+ *********************************************************************************
+ */
+
+int wiringPiSPISetupMode (int channel, int speed, int mode)
+{
+  int fd ;
+
+  mode    &= 3 ;	// Mode is 0, 1, 2 or 3
+  channel &= 1 ;	// Channel is 0 or 1
+
+  if ((fd = open (channel == 0 ? spiDev0 : spiDev1, O_RDWR)) < 0)
+    return wiringPiFailure (WPI_ALMOST, "Unable to open SPI device: %s\n", strerror (errno)) ;
+
+  spiSpeeds [channel] = speed ;
+  spiFds    [channel] = fd ;
+
+// Set SPI parameters.
+
+  if (ioctl (fd, SPI_IOC_WR_MODE, &mode)            < 0)
+    return wiringPiFailure (WPI_ALMOST, "SPI Mode Change failure: %s\n", strerror (errno)) ;
+  
+  if (ioctl (fd, SPI_IOC_WR_BITS_PER_WORD, &spiBPW) < 0)
+    return wiringPiFailure (WPI_ALMOST, "SPI BPW Change failure: %s\n", strerror (errno)) ;
+
+  if (ioctl (fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed)   < 0)
+    return wiringPiFailure (WPI_ALMOST, "SPI Speed Change failure: %s\n", strerror (errno)) ;
+
+  return fd ;
+}
+
+
+int initSPI(){
+ //return wiringPiSPISetupMode (channel, speed, 0) ;
+return wiringPiSPISetupMode (0, 20000, 0) ;
 }
 
 unsigned char writeSPI(char data_out){
