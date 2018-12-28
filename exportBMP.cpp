e5ed63d8f2e3938c53418aaddbd1e03171157b9c
Ken <tranthamkw@unk.edu>
2018-12-28 14:02:37 -0600

    updated export bmp to auto generate an h file

--- exportBMP.cpp
+++ exportBMP.cpp
@@ -1,5 +1,9 @@
 /*
-*  test area for using bitmap.cpp
+
+Program to convert a 128x128x24bit Device independent Bitmap into a data header file useable by 
+
+a pic to display an image. 
+
 *
 */
 
@@ -19,7 +23,7 @@ int main (int nNumberofArgs, char* argv[]){
    
 FILE *fp;
 char fileName[32];
-unsigned int width,height,i,j;
+unsigned int width,height,i,j,bpp;
 unsigned int color;
 unsigned char red, green, blue;
 unsigned char pixel[3];
@@ -33,13 +37,19 @@ if (nNumberofArgs==3){
 
 	if(sourcebmap->loaded){
 
-	strcpy(fileName,argv[2]);
-	fp=fopen(fileName,"w");
 
 	height=sourcebmap->height;
 	width=sourcebmap->width;
-	printf("height %d \twidth %d\n",height,width);
-	
+	bpp = sourcebmap->bpp;
+
+	printf("height %d \twidth %d\tBPP %d\n",height,width,bpp);
+
+	if (height==128 & width==128 & bpp==24){
+		strcpy(fileName,argv[2]);
+		fp=fopen(fileName,"w");
+		fprintf(fp,"/* image is a 128x128 pixel array encoded  with 4 bits per color.\n");
+fprintf(fp,"data:  image[i]=[RG], image[i+1]=[BR], image [i+2]=[GB] corresponding to pixel[m] and pixel[m+1]. There are 192 bytes per line and 128 lines.\n*/\n");
+		fprintf(fp,"const char image[]={\n");
 	for (i=0;i<height;i++){
 		for(j=0;j<width;j++){
 		color=sourcebmap->getPixel(i,j);
@@ -52,21 +62,31 @@ if (nNumberofArgs==3){
 		//odd
 			pixel[1] = (pixel[1] & 0xF0) | red;
 			pixel[2]= (green<<4) | blue;
-		printf("%02X%02X%02X,",pixel[0],pixel[1],pixel[2]);
-		fprintf(fp,"%d,%d,%d,",pixel[0],pixel[1],pixel[2]);
+//		printf("%02X%02X%02X,",pixel[0],pixel[1],pixel[2]);
+		fprintf(fp,"%d,%d,%d",pixel[0],pixel[1],pixel[2]);
+
+		if (i==127) {
+			if (j==127){
+				fprintf(fp,"};\n");}
+		}else{
+			if (j==127){
+			fprintf(fp,",\n");
+			} else {
+			fprintf(fp,",");
+			}
+		}
+
+
 		} else {
 		//even
 			pixel[0]=(red<<4) | green;
 			pixel[1]= (pixel[1] & 0x0F) | (blue<<4);
-		}		
+		}
+	      } // for j
 
-		if ((j % 64) ==0) {
-		printf("\n");
-		fprintf(fp,"\n");
-	}
+	}// for i
 
-      }
-	}
+	}// if height & width & bpp
 
 	fclose(fp); 
 } else {
@@ -74,7 +94,7 @@ printf("error loading bmp\n");
 }
 } else {
 
-printf("Usage::  ./exportBMP <source.bmp> <dataout.txt>\n");
+printf("Usage::  ./exportBMP <source.bmp> <imageheaderfile.h>\n");
 
 }
 
